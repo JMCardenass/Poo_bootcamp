df16b1f984d346e7d96b14121884bbbb8af80980
JMCardenass <jmcardenas1807@gmail.com>
2023-05-10 21:18:51 -0500

    feat: implementacion de la encapsulacion

--- Animales.py
+++ Animales.py
@@ -1,9 +1,11 @@
 class Animales:
     def __init__(self, raza , edad , color):
-        self.raza = raza
+        self._raza = raza
         self.edad = edad
         self.color = color
         self.is_comer = False
+    def get_raza(self):
+        return self._raza
     def start_comer(self):
         self.is_comer = True
         print("El animal esta comiendo")
@@ -17,7 +19,7 @@ class Animales:
     def habla(self):
         pass
 mojito = Animales("conejo", "2", "blanco")  
-print(mojito.raza, mojito.edad, mojito.color)
+print(mojito.get_raza(), mojito.edad, mojito.color)
 mojito.start_comer()
 mojito.salio_corriendo()
 mojito.cumple_años()
@@ -36,11 +38,8 @@ class Gato(Animales):
     def habla(self):
         return"miau"
 Romeo = Gato("persa", "3", "blanco", "Romeo")
-print(Romeo.raza, Romeo.edad, Romeo.color, Romeo.nombre)
+print(Romeo.get_raza, Romeo.edad, Romeo.color, Romeo.nombre)
 Romeo.start_comer() 
 Romeo.salio_corriendo()
 Romeo.cumple_años()
 print(Romeo.habla())
-
-    
-
